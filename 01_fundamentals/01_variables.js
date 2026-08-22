6bf825509e18c4c040148f870059ced8c016f385
tiwarisuraj010 <tiwarisuraj11019@gmail.com>
2026-08-22 20:27:25 +0530

    \Initializing git repository

---
+++ 01_fundamentals/01_variables.js
@@ -0,0 +1,26 @@
+
+// What is variable ? 
+
+// A variable is a name binding to a value. If the value is an object, the binding provides access to that object.
+
+//  Declaration 
+let a;   // Declaration of binding or variable
+ a = 10;  // Assigning a value to variable
+
+let place ;
+place = 'Gorkha'
+
+// Initialization 
+
+//means Declaration + Assignment together 
+
+let name = "Sarthak";
+
+Note : "We cannot declare  const variable without Initializing it "
+
+// Difference between var, let and const
+
+let v = 10 ;   // let creates an variable that can be reassigned
+const  x = 10 ; // const variables can't be reassigned they are initialized once and remain fixedc
+
+var less_used = "right" ; // this variable is old and its valeus can also be reassigned but because of scope problem this is not used widely in code.
---
+++ 01_fundamentals/02_data_types.js
@@ -0,0 +1,34 @@
+// JavaScript DataTypes 
+
+/* JavaScript has two types of Data types  
+
+1. Primitive          2. Object or Reference type        */
+
+// Primitive type
+
+let a = "Sarthak"; //String
+let b =  12 ;    // Integer
+let c =   12.5 ;  // Float
+let d =    true;  // Boolean
+let e =  undefined  ;   // Undefined
+let f =    null ;      // null
+let g =  72927598157957195759147517435914359431543198;  // bigint
+let h = "symbol" ;     // symbol
+
+
+// Object or Reference type
+
+let nums1 = [1,2,3,4,5]  // Array
+let nums2 = "a" ;   // object 
+
+// to check type
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+console.log("Data Type:",typeof(f));
+console.log(typeof(g));
+console.log(typeof(h));
+
